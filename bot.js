5cbac7c20f17ff35a6787fc75c47fdff44525470
arc25275 <arc25275@gmail.com>
2020-12-15 15:39:26 -0800

    remove dotenv

--- bot.js
+++ bot.js
@@ -1,10 +1,7 @@
-require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const TOKEN = process.env.TOKEN;
-
-bot.login(TOKEN);
+bot.login(NzUwOTQ1NDU5NTk3NjA2OTMz.X1B6dQ.Tf4V9EhKhFFGOoipPw4nnzlYoIw);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);

7ae960f15646574fb1b176bc6a6b067f5d130a7e
arc25275 <arc25275@gmail.com>
2020-12-15 15:33:15 -0800

    change to discord.js

--- bot.js
+++ bot.js
@@ -1,34 +1,18 @@
-var Discord = require('discord.js');
-var logger = require('winston');
-var auth = require('./auth.json');
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-   colorize: true
-});
-logger.level = 'debug';
+const TOKEN = process.env.TOKEN;
 
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
-});
-bot.on('ready', function (evt) {
-   logger.info('Connected');
-   logger.info('Logged in as: ');
-   logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-   if (message.substring(0, 1) == '!') {
-       var args = message.substring(1).split(' ');
-       var cmd = args[0];
-       args = args.splice(1);
-       switch(cmd) {
-           case 'Hello':
-               bot.sendMessage({
-                   to: channelID,
-                   message: 'a'
-               });
-           break;
-        }
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+  });
+
+  bot.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+      msg.channel.send('pong');
     }
-});
+  });

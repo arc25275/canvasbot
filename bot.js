a77e31cfff74c4a7b0adce4008ab9ab187b17a35
arc25275 <arc25275@gmail.com>
2020-12-15 19:20:42 -0800

    First Version of the assignment command, and moved tokens to a json file

--- bot.js
+++ bot.js
@@ -1,15 +1,44 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
+const fetch = require("node-fetch");
+const TurndownService = require("turndown");
+const turndownService = new TurndownService();
+const TOKENS = require("./auth.json");
+const prefix = ".";
 
-bot.login(NzUwOTQ1NDU5NTk3NjA2OTMz.X1B6dQ.Tf4V9EhKhFFGOoipPw4nnzlYoIw);
+const body = { a: 1 };
+bot.login(TOKENS.BOT_TOKEN);
 
-bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
-  });
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
 
-  bot.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-      msg.channel.send('pong');
+  bot.on("message", (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === "assignment") {
+      async function getAssignment() {
+        let link = args[0];
+        const links = link.split("/courses");
+        const apiLink = links[0] + "/api/v1/courses" + links[1];
+        const result = await fetch(
+          apiLink.concat("?access_token=" + TOKENS.CANVAS_TOKEN)
+        ).then((res) => res.json());
+        var embedDescription = turndownService.turndown(result.description);
+        embedDescription = embedDescription.replace(/#+/g, "");
+        const assignmentEmbed = new Discord.MessageEmbed()
+          .setColor("#0099ff")
+          .setTitle(result.name)
+          .setURL(link)
+          .setDescription(embedDescription)
+          .setFooter(result.due_at);
+
+        message.channel.send(assignmentEmbed);
+      }
+      getAssignment();
     }
   });
+});
